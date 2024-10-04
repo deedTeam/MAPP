e7a4980f207ce212b080823b3afce8c4eed6c177
Piotr <oskwarek.piotrek@gmail.com>
2024-10-04 11:37:04 +0200

    created-properties-component

--- frontend/src/app/app-panel/element-properties/element-properties.component.ts
+++ frontend/src/app/app-panel/element-properties/element-properties.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PropertiesComponent } from "./properties/properties.component";
 
 @Component({
   selector: 'app-element-properties',
   standalone: true,
-  imports: [],
+  imports: [PropertiesComponent],
   templateUrl: './element-properties.component.html',
   styleUrl: './element-properties.component.css'
 })
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileBarComponent } from "./app-panel/profile-bar/profile-bar.component";
+import { ElementPropertiesComponent} from "./app-panel/element-properties/element-properties.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileBarComponent, ElementPropertiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
