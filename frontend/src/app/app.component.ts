f5d9986880b7e043243251f21f5bf4872d95a48d
Piotr <oskwarek.piotrek@gmail.com>
2024-10-07 18:41:26 +0200

    feat: initial version of element properties and toolbar UI added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileBarComponent } from "./app-panel/profile-bar/profile-bar.component";
 import { ElementPropertiesComponent} from "./app-panel/element-properties/element-properties.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileBarComponent, ElementPropertiesComponent],
+  imports: [RouterOutlet, ProfileBarComponent, ElementPropertiesComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
