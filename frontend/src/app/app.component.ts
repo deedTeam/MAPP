d84f557cf2d30cb84ef85e5f99b70195d7622575
Piotr <oskwarek.piotrek@gmail.com>
2024-10-09 21:39:32 +0200

    feat: map added. fix the tiles if placed wrongly

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileBarComponent } from "./app-panel/profile-bar/profile-bar.component";
 import { ElementPropertiesComponent} from "./app-panel/element-properties/element-properties.component";
 import { ToolbarComponent } from "./toolbar/toolbar.component";
+import {MapComponent} from "./map/map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileBarComponent, ElementPropertiesComponent, ToolbarComponent],
+  imports: [RouterOutlet, ProfileBarComponent, ElementPropertiesComponent, ToolbarComponent, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/map/map.component.ts
@@ -0,0 +1,21 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent implements AfterViewInit {
+map:any;
+
+  ngAfterViewInit() {
+    this.map = L.map('map').setView([51.867603, 19.520680], 7);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    }).addTo(this.map);
+  }
+}
